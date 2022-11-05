8189b3cc5c4eea445b6b869cd4271033c06dedc4
oscar.salazar <oscar.salazar@ceiba.com.co>
2022-11-05 14:10:12 -0500

    se añade parámetro para ignorar advertencias del mapper

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/product/catalog/repository/product/data/ProductMapper.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/product/catalog/repository/product/data/ProductMapper.java
@@ -2,8 +2,9 @@ package co.com.product.catalog.repository.product.data;
 
 import co.com.product.catalog.model.product.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductMapper {
     Product toEntity(ProductData product);
     ProductData toData(Product product);
